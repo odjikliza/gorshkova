d8ea7b37582449d9f103871730867e725a5ccfd6
odjikliza <158804629+odjikliza@users.noreply.github.com>
2024-10-30 21:18:43 +0300

    Update 1.py

--- лабораторная работа2/1.py
+++ лабораторная работа2/1.py
@@ -8,9 +8,10 @@ months = 0
 current_money = money_capital + salary
 
 while current_money >= spend:
-  months += 1
-  current_money -= spend
-  spend *= (1 + increase)
-  current_money += salary
+    months += 1
+    current_money -= spend
+    spend *= (1 + increase)
+    current_money += salary
 
 print("Количество месяцев, которое можно протянуть без долгов:", months)
+
