5a2fc3ca494d44fd1b16f52ab6fb9c5157c86725
odjikliza <158804629+odjikliza@users.noreply.github.com>
2024-11-09 00:53:04 +0300

    Update 1.py

--- лабораторная работа3/1.py
+++ лабораторная работа3/1.py
@@ -25,7 +25,3 @@ for find_item in ['банан', 'груша', 'персик']:
         print(f"Первое вхождение товара '{find_item}' имеет индекс {index_item}.")
     else:
         print(f"Товар '{find_item}' не найден в списке.")
-
-
-
-
