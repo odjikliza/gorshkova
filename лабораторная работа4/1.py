81fe145cdb7a021a3a7ed03367e942213c468b54
odjikliza <158804629+odjikliza@users.noreply.github.com>
2024-11-13 22:28:28 +0300

    Update 1.py

--- лабораторная работа4/1.py
+++ лабораторная работа4/1.py
@@ -1,49 +1,15 @@
 import json
 
-data = """
-[
-  {
-    "score": 0.0009456152645028281,
-    "weight": 1
-  },
-  {
-    "score": 0.00020640167757499364,
-    "weight": 1
-  },
-  {
-    "score": 0,
-    "weight": 1
-  },
-  {
-    "score": 1.6557065217391307,
-    "weight": 1
-  },
-  {
-    "score": 0,
-    "weight": 1
-  },
-  {
-    "score": 0.6066065217391303,
-    "weight": 1
-  },
-  {
-    "score": 0.03126181644071977,
-    "weight": 1
-  },
-  {
-    "score": 0.001253973281817707,
-    "weight": 1
-  }
-]
-"""
+INPUT_FILE = 'input.json'
 
 
-def task():
-    json_data = json.loads(data)
+def task(filename):
+    with open(filename) as file:
+        json_data = json.load(file)
 
     total = sum(item['score'] * item['weight'] for item in json_data)
 
     return round(total, 3)
 
 
-print(task())
+print(task(INPUT_FILE))
